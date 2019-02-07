ba572af9e10c726d27d848101892b910b1b1648a
Arnoldas <arnuske@yahoo.com>
2019-02-07 16:35:09 +0200

    Inserted Data

--- src/Command/ParseJsonUsers.php
+++ src/Command/ParseJsonUsers.php
@@ -2,12 +2,27 @@
 
 namespace App\Command;
 
+use App\Entity\Users;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ParseJsonUsers extends Command
 {
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+    }
+
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'app:parse-users';
 
@@ -25,20 +40,33 @@ class ParseJsonUsers extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+
+
         $content=file_get_contents("https://gist.githubusercontent.com/emodus/27d245484a85c2286722b9d146c53354/raw/c9af224580a22cbde969127527c4500e3f7d2a9e/dummyFeed");
         $data=json_decode($content, true);
 
-        $connect = pg_connect("host=localhost port=5432 dbname=db_name user=postgres password=db_password");
+
 
         foreach ($data['items'] as $row)
         {
-            $sql = "INSERT INTO users(index, index_start_at, some_number, floater,
-                  first_name, surname, fullname, email, bully) VALUES ('".$row["index"]."','".$row["index_start_at"]."', '".$row["integer"]."',
-                  '".$row["float"]."'',''".$row["name"]."','".$row["surname"]."','".$row["fullname"]."',
-                  '".$row["email"]."','".$row["bool"]."') ";
+            //var_dump($row);die;
+            $user = new Users();
+            $user->setIndex($row['index']);
+            $user->setIndexStartAt($row['index_start_at']);
+            $user->setSomeNumber($row['integer']);
+            $user->setFloater($row['float']);
+            $user->setFirstName($row['name']);
+            $user->setSurname($row['surname']);
+            $user->setFullname($row['fullname']);
+            $user->setEmail($row['email']);
+            $user->setBully($row['bool']);
 
-            pg_query($connect, $sql);
+            // tell Doctrine you want to (eventually) save the Product (no queries yet)
+            $this->entityManager->persist($user);
+
         }
+        // actually executes the queries (i.e. the INSERT query)
+        $this->entityManager->flush();
 
         echo "ALL DATA INSERTED";
     }
